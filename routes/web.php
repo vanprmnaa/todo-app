6ecd378bdc472cface6fd2c172aa1ec41dab0e89
vanprmnaa <revanpermana365@gmail.com>
2025-02-17 11:08:44 +0700

    melengkapi fitur about dan menambahkan fitur edit

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AboutController;
 
-
-
-
-
 Route::resource('lists', TaskListController::class);
 Route::resource('tasks', TaskController::class);
 Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+Route::patch('/tasks/{task}/change-list', [TaskController::class, 'changeList'])->name('tasks.changeList');
 
 // Membuat route untuk home
 Route::get('/', [TaskController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
-Route::get('/contact', [ContactController::class, 'index'])->name('contact');
