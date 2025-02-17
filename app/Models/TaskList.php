166c5398e77073c4f7fe35a8478fd2fc02cd36ac
vanprmnaa <revanpermana365@gmail.com>
2025-02-17 18:30:57 +0700

    Menambahkan komentar di TaskList model

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -4,33 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Model untuk tabel task_lists
- *
- * @property int $id
- * @property string $name
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Database\Eloquent\Collection|Task[] $tasks
- * @property int|null $tasks_count
- */
 class TaskList extends Model
 {
+    // Kolom yang dapat diisi
     protected $fillable = ['name'];
+
+    // Kolom yang tidak dapat diisi
     protected $guarded = [
         'id',
         'created_at',
         'updated_at'
     ];
 
-    /**
-     * Relasi dengan model Task
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
+    // Fungsi untuk mengambil task
     public function tasks()
     {
+        // Relasi ke class Task untuk mendapatkan data tasks yang list_id nya sesuai dengan id dari list
         return $this->hasMany(Task::class, 'list_id');
     }
 }
-
