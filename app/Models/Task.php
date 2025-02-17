8a25a319f774a8b8264726eb8060e333addad057
vanprmnaa <revanpermana365@gmail.com>
2025-02-17 18:30:37 +0700

    Menambahkan komentar di Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,11 +10,7 @@ use App\Models\TaskList;
  */
 class Task extends Model
 {
-    /**
-     * Kolom yang dapat diisi
-     *
-     * @var array
-     */
+    // Kolom yang dapat diisi
     protected $fillable = [
         'name',
         'description',
@@ -23,35 +19,24 @@ class Task extends Model
         'list_id'
     ];
 
-    /**
-     * Kolom yang tidak dapat diubah
-     *
-     * @var array
-     */
+    // Kolom yang tidak dapat diisi atau diubah
     protected $guarded = [
         'id',
         'created_at',
         'updated_at'
     ];
 
-    /**
-     * List of priorities
-     *
-     * @var array
-     */
+    // Constanta warna untuk prioritas task
     const PRIORITIES = [
         'low',
         'medium',
         'high'
     ];
 
-    /**
-     * Mengembalikan class berdasarkan prioritas
-     *
-     * @return string
-     */
-    public function getPriorityClassAttribute() {
-        return match($this->attributes['priority']) {
+    // Fungsi untuk mengambil class warna berdasarkan prioritas task
+    public function getPriorityClassAttribute()
+    {
+        return match ($this->attributes['priority']) {
             'low' => 'success',
             'medium' => 'warning',
             'high' => 'danger',
@@ -59,13 +44,10 @@ class Task extends Model
         };
     }
 
-    /**
-     * Relasi dengan TaskList
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function list() {
+    // Fungsi untuk mengambil list task
+    public function list()
+    {
+        // Relasi ke class TaskList untuk mengambil data list berdasarkan list_id dari task
         return $this->belongsTo(TaskList::class, 'list_id');
     }
 }
-
