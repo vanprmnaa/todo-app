4ccceff65ecf675f17e082a8f5401a740847bbb3
vanprmnaa <revanpermana365@gmail.com>
2025-02-17 18:30:25 +0700

    Menambahkan komentar di TaskListController

--- app/Http/Controllers/TaskListController.php
+++ app/Http/Controllers/TaskListController.php
@@ -10,36 +10,30 @@ use Illuminate\Http\Request;
  */
 class TaskListController extends Controller
 {
-    /**
-     * Membuat task list baru.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    // Fungsi/Logika untuk menambahkan List
     public function store(Request $request)
     {
+        // Validasi data yang dikirim dari form
         $request->validate([
-            'name' => 'required|max:100',
+            'name' => 'required|max:100', // Harus diisi dan Maksimal 100 karakter
         ]);
 
+        // Tambahkan jika data ke database jika berhasil divalidasi
         TaskList::create([
             'name' => $request->name,
         ]);
 
+        // Kembali ke halaman sebelumnya jika data berhasil ditambahkan
         return redirect()->back();
     }
 
-    /**
-     * Menghapus task list.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // Fungsi/Logika untuk menghapus List
     public function destroy($id)
     {
+        // Cari List yang ingin dihapus, jika ketemu hapus, jika tidak tampilkan halaman 404
         TaskList::findOrFail($id)->delete();
 
+        // Jika List berhasil dihapus arahkan kembali ke halaman sebelumnya
         return redirect()->back();
     }
 }
-
